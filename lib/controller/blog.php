0bb70ab3297e4fafb4b6070b6bf6990f1f8f5f9d
limstash <limstash.w@gmail.com>
2020-01-30 20:52:39 +0800

    bug fix: delete comments, tags...etc with delete blog

--- lib/controller/blog.php
+++ lib/controller/blog.php
@@ -29,7 +29,13 @@
 
         log::writelog(2, 3, 306, "删除博客 {$post["title"]}");
 
-        db::query("oj", "delete from blogs where id = '$id'");
+    	db::query("oj", "delete from click_zans where type = 'B' and target_id = $id");
+        db::query("oj", "delete from click_zans where type = 'BC' and target_id in (select id from blogs_comments where blog_id = $id)");
+        db::query("oj", "delete from blogs where id = $id");
+        db::query("oj", "delete from blogs_comments where blog_id = $id");
+        db::query("oj", "delete from important_blogs where blog_id = $id");
+        db::query("oj", "delete from blogs_tags where blog_id = $id");
+        
         auth::sendMessage($post["poster"], "您发布的博客已被删除", "您于【{$post["post_time"]}】发布的《{$post["title"]}》因【{$comments}】已被管理员删除，如有疑问请于管理员联系！");
     } else {
         exit("");
